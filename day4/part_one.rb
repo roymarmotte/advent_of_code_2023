d5cbbed9e1798c11963d4ff772029bf0a6035dcb
Valentin Maeght <valentin@hop3team.com>
2023-12-04 21:28:39 +0100

    💥 Part 1 day 4 done

---
+++ day4/part_one.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+input = File.readlines('./input.txt')
+
+results = input.map do |line|
+  _game_name, game = line.split(':')
+  winning_nb, drawn_nb = game.split('|')
+
+  winning_nb = winning_nb.scan(/\d+/).map(&:to_i)
+  drawn_nb = drawn_nb.scan(/\d+/).map(&:to_i)
+
+  good_nb = drawn_nb.select { |nb| winning_nb.include? nb }
+  good_nb.each_index.map { |key, _value| key.zero? ? 1 : 2 }.inject(:*)
+end
+pp results.compact.sum
