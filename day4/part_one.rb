343f352925cc546a17c02f8e860b632cbb644065
Valentin Maeght <valentin@hop3team.com>
2023-12-04 22:39:26 +0100

    ⚡️  use & operator to compare arrays

--- day4/part_one.rb
+++ day4/part_one.rb
@@ -9,7 +9,7 @@ results = input.map do |line|
   winning_nb = winning_nb.scan(/\d+/).map(&:to_i)
   drawn_nb = drawn_nb.scan(/\d+/).map(&:to_i)
 
-  good_nb = drawn_nb.select { |nb| winning_nb.include? nb }
+  good_nb = drawn_nb & winning_nb
   good_nb.each_index.map { |key, _value| key.zero? ? 1 : 2 }.inject(:*)
 end
 pp results.compact.sum
